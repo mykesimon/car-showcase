5f4cd28cdbe2a08f45c8942c2ef094915ce4fb7f
Myke Simon <hi@mykesimon.com>
2023-11-24 16:00:56 +0000

    Add car list and modals

---
+++ components/SearchButton.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import React from 'react';
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => {
+	return (
+		<button
+			className={`-ml-3 z-10 ${otherClasses}`}
+			type='submit'
+		>
+			<Image
+				src='/magnifying-glass.svg'
+				alt='magnifying glass'
+				width={40}
+				height={40}
+				className='object-contain'
+			/>
+		</button>
+	);
+};
+
+export default SearchButton;
